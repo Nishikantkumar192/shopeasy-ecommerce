edfd5d00a03187582b93b24ecede7d0741ea3c7f
nishi kant kumar <nishikants192@gmail.com>
2026-04-04 03:47:50 +0530

    fix push issue

--- client/src/pages/ParticularProductDetails.jsx
+++ client/src/pages/ParticularProductDetails.jsx
@@ -3,12 +3,6 @@ import NoteContext from "../context/NoteContext"
 import { toast } from "react-toastify";
 import api from "../api/axios";
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
-
-const ParticularProductDetails = () => {
-  const {id}=useParams();
-  const { specificItem,getSpecificDetail } = useContext(NoteContext);
-=======
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
@@ -16,16 +10,12 @@ const ParticularProductDetails = () => {
   const navigate=useNavigate();
   const {id}=useParams();
   const { specificItem,getSpecificDetail,deleteProduct } = useContext(NoteContext);
->>>>>>> 3a7ff32 (feat: enhance product listing UI and implement add-to-cart functionality)
   useEffect(()=>{
     getSpecificDetail(id);
   })
 
   const addToCart=async()=>{
-<<<<<<< HEAD
-=======
     navigate(`/cart-items/${id}`);
->>>>>>> 3a7ff32 (feat: enhance product listing UI and implement add-to-cart functionality)
     try{
       const {data}=await api.post(`/api/cart/addToCart/${specificItem._id}`);
       toast.success(data.message);
@@ -63,15 +53,10 @@ const ParticularProductDetails = () => {
 
           {/* buttons */}
           <div>
-<<<<<<< HEAD
-            <button className="bg-yellow-500 p-2 hover:opacity-80 rounded-lg text-xl" onClick={()=>addToCart()}>Add To Cart</button>&nbsp; &nbsp;
-            <button className="bg-orange-400 p-2 hover:opacity-80 rounded-lg text-xl" onClick={()=>BuyNow()}>Buy Now</button>
-=======
             <button className="bg-yellow-500 p-2 hover:opacity-80 rounded-lg text-xl m-4" onClick={()=>addToCart()}>Add To Cart</button>
             <button className="bg-orange-400 p-2 hover:opacity-80 rounded-lg text-xl m-4" onClick={()=>BuyNow()}>Buy Now</button>
             <Link className="bg-green-600 p-2 hover:opacity-80 rounded-lg text-xl m-4" to={`/updateItemInformation/${specificItem._id}`}>Update Details</Link>
             <button className="bg-black text-white p-2 hover:opacity-80 rounded-lg text-xl m-4" onClick={()=>deleteProduct(specificItem._id)}>Delete product</button>      
->>>>>>> 3a7ff32 (feat: enhance product listing UI and implement add-to-cart functionality)
           </div>
         </div>
       </div>
