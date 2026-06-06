f746be8164735befe122c5b798cb512280383c61
nishi kant kumar <nishikants192@gmail.com>
2026-06-06 08:40:19 +0530

    implement frontend order status update functionality

--- client/src/pages/DisplaySpecificOrderDetails.jsx
+++ client/src/pages/DisplaySpecificOrderDetails.jsx
@@ -6,6 +6,7 @@ import ShowProduct from "../components/ShowProduct";
 
 const DisplaySpecificOrderDetails = () => {
   const [details,setDetails]=useState({});
+  const [visibility,setVisibility]=useState(false);
   const { id } = useParams();
   useEffect(() => {
     userOrderDetails();
@@ -18,19 +19,28 @@ const DisplaySpecificOrderDetails = () => {
       toast.error(err.response?.data?.message);
     }
   };
+  const handleVisibility=()=>{
+    setVisibility(visibility?false:true);
+  }
   return (
-    <div className="flex flex-wrap flex-col mt-12 p-4 min-h-screen bg-pink-400">
-      <div className="w-full text-center text-3xl">Order_owner : {details?.relatedUser?.username}</div>
+    <div className="flex flex-wrap items-center flex-col mt-12 p-4 min-h-screen bg-pink-400">
+      <div className="w-full text-center text-3xl mb-10">Order_owner : {details?.relatedUser?.username}</div>
       <span className="text-2xl">Amount: &#8377;{details.amount}</span>
-      <span className="text-xl">Order_Status: {details.orderStatus}</span>
+      <span className="text-xl cursor-pointer underline" onClick={()=>handleVisibility()}>Order_Status: {details.orderStatus}</span>
+      {visibility && <select className="outline-none text-2xl" name="status">
+        <option value="">Change order status</option>
+        <option value="pending">Pending</option>
+        <option value="processing">Processing</option>
+        <option value="shipped">Shipped</option>
+        <option value="delivered">Delivered</option>
+      </select>}
       <span className="text-xl">Payment_status : {details.paymentStatus}</span>
       <span className="text-xl">Address : {details.relatedUser?.address}</span>
       <span className="text-xl">Phone_number : {details.relatedUser?.phone_no}</span>
-      <div className="flex flex-wrap gap-12 mt-8 items-center">
+      <div className="flex flex-wrap justify-center gap-12 pt-20 ">
       {details?.products?.map((product)=>{
         return <ShowProduct item={product.productId} key={product._id}/>
       })}
-      {console.log(details.products)}
       </div>
     </div>
   );
